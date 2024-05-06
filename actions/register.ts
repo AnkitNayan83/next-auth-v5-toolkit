4fb72a05d7b78732864db064a1e0ca0a9c62e740
AnkitNayan83 <ankitnayan83@gmail.com>
2024-05-06 15:56:48 +0530

    commit

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,8 @@
 
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -9,6 +11,28 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return { error: "Invalid Field" };
     }
+
+    const { email, name, password } = validatedFields.data;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const userEmailCheck = await db.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (userEmailCheck) {
+        return { error: "Email already exists. Please login if this is your email" };
+    }
+
+    await db.user.create({
+        data: {
+            email,
+            hashedPassword,
+            name,
+        },
+    });
 
     return { success: "Account created" };
 };
