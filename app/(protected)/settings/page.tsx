4710e1ded3bd8ebb3c78e86c76b3a209e5721082
AnkitNayan83 <ankitnayan83@gmail.com>
2024-05-08 19:40:21 +0530

    added callbacks

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,10 +1,23 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth"; //for server components import from @/auth
+import { Button } from "@/components/ui/button";
 const page = async () => {
     const session = await auth();
 
     return (
         <div>
             <h1 className="text-2xl font-bold">{JSON.stringify(session)}</h1>
+            {
+                // for server components
+            }
+            <form
+                action={async () => {
+                    "use server";
+
+                    await signOut();
+                }}
+            >
+                <Button>Sign Out</Button>
+            </form>
         </div>
     );
 };
--- auth.ts
+++ auth.ts
@@ -2,9 +2,42 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    ...authConfig,
+    // What action will be performed after a certain event happens like signIn ...
+    callbacks: {
+        // To extend the session you have to return JWT token first
+        async session({ session, token }) {
+            // console.log({ "Session Token": { token }, session });
+            // if (session.user) {
+            //     session.user.customField = token.customField;
+            // }
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+            }
+
+            if (token.role && session.user) {
+                //@ts-ignore
+                session.user.role = token.role;
+            }
+            return session;
+        },
+        async jwt({ token }) {
+            // console.log({ token });
+            // token.customField = "Ankit";
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) return token;
+
+            token.role = existingUser.role;
+
+            return token;
+        },
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
+    ...authConfig,
 });
